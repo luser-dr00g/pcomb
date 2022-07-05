07c60dea5ad2d9ee68ea05c6a1c1f4c38714720d
M. Joshua Ryan <luser.droog@gmail.com>
2022-07-04 23:09:46 -0500

    new file

--- proto_pc11ctoken.h
+++ proto_pc11ctoken.h
@@ -4,25 +4,55 @@
 #endif
 
 #define Each_Symbolic(_) \
-  _("int", kw_int) _("char", kw_char) \
+  _("int", kw_int) _("char", kw_char)                                \
   _("float", kw_float) _("double", kw_double) _("struct", kw_struct) \
-  _("auto", kw_auto) _("extern", kw_extern) \
-  _("register", kw_register) _("static", kw_static) \
-  _("goto", kw_goto) _("return", kw_return) _("sizeof", kw_sizeof) \
-  _("break", kw_break) _("continue", kw_continue) \
-  _("if", kw_if) _("else", kw_else) \
-  _("for", kw_for) _("do", kw_do) _("while", kw_while) \
+  _("auto", kw_auto) _("extern", kw_extern)                          \
+  _("register", kw_register) _("static", kw_static)                  \
+  _("goto", kw_goto) _("return", kw_return) _("sizeof", kw_sizeof)   \
+  _("break", kw_break) _("continue", kw_continue)                    \
+  _("if", kw_if) _("else", kw_else)                                  \
+  _("for", kw_for) _("do", kw_do) _("while", kw_while)               \
   _("switch", kw_switch) _("case", kw_case) _("default", kw_default) \
-  /*_("entry", kw_entry) */ \
-  _("*", op_star) _("++", op_inc) _("+", op_plus) _(".", op_dot) \
-  _("->", op_arrow) _("--", op_dec) _("-", op_minus) \
-  _("!=", op_neq) _("!", op_bang) _("~", op_tilde) \
-  _("&&", op_andand) _("&", op_and) _("==", op_eqeq) _("=", op_eq) \
-  _("^", op_caret) _("||", op_oror) _("|", op_or) \
-  _("/", op_slant) _("%", op_percent) \
-  _("<<", op_left) _("<=", op_leq) _("<", op_lt) \
-  _(">>", op_right) _(">=", op_geq) _(">", op_gt) \
-  _("(", lpar) _(")", rpar) \
-  _(",", comma) _(";", semi) _(":", colon) _("?", quest) \
-  _("{", lbrace) _("}", rbrace) _("[", lbrack) _("]", rbrack) \
+  /*_("entry", kw_entry) */                                          \
+  _("*", op_star) _("++", op_inc) _("+", op_plus) _(".", op_dot)     \
+  _("->", op_arrow) _("--", op_dec) _("-", op_minus)                 \
+  _("!=", op_neq) _("!", op_bang) _("~", op_tilde)                   \
+  _("&&", op_andand) _("&", op_and) _("==", op_eqeq) _("=", op_eq)   \
+  _("^", op_caret) _("||", op_oror) _("|", op_or)                    \
+  _("/", op_slant) _("%", op_percent)                                \
+  _("<<", op_left) _("<=", op_leq) _("<", op_lt)                     \
+  _(">>", op_right) _(">=", op_geq) _(">", op_gt)                    \
+  _("(", lpar) _(")", rpar)                                          \
+  _(",", comma) _(";", semi) _(":", colon) _("?", question)          \
+  _("{", lbrace) _("}", rbrace) _("[", lbracket) _("]", rbracket)    \
 //end Each_Symbolic
+
+#define Each_assignop(_) \
+  _("+=", op_pluseq) _("-=", op_minuseq)                       \
+  _("*=", op_stareq) _("/=", op_slanteq) _("%=", op_percenteq) \
+  _(">>=", op_righteq) _("<<=", op_lefteq)                     \
+  _("&=", op_andeq) _("^=", op_careteq) _("|=", op_pipeeq)
+
+#define Semantic_Tokens(_) \
+  _(ident) _(const_int) _(const_float) _(const_char) _(const_string)
+
+#define Languages(_) \
+  _(C90) _(C99)
+
+#define Symbolic_name(x,y) y ,
+#define Semantic_name(x) x ,
+#define Semantic_string(x) #x
+#define Language_name(x) LANG_ ## x ,
+
+enum token_symbol_codes {
+  token_symbols = END_PARSER_SYMBOLS,
+  Semantic_Tokens( Semantic_name )
+  Each_Symbolic( Symbolic_name )
+  Each_assignop( Symbolic_name )
+  Languages( Language_name )
+  END_TOKEN_SYMBOLS
+};
+ 
+typedef enum language { Languages( Semantic_name ) } language;
+
+list tokens_from_chars( language lang, list input );
